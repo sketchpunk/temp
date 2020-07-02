4c89307acc526fcbf3e63c237797d0fd132a20ed
Pedro Sousa <pedro.sousa@probussys.com>
2020-07-02 19:32:02 -0400

    New Toon Shader

---
+++ Fungi_v5_5/_test/shading/_links.js
@@ -0,0 +1,73 @@
+https://ciechanow.ski/lights-and-shadows/ INSANE VISUALS
+
+
+https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading
+https://www.scratchapixel.com/lessons/3d-basic-rendering/phong-shader-BRDF
+
+https://www.clicktorelease.com/blog/creating-spherical-environment-mapping-shader/
+
+https://www.gsn-lib.org/docs/gallery.php?projectName=RaymarchingSDF&graphName=SignedDistanceField3D
+
+https://alain.xyz/blog/advances-in-material-models
+
+https://gamedev.stackexchange.com/questions/129139/how-do-i-calculate-uv-space-from-world-space-in-the-fragment-shader
+
+// PERFECT, WORLD SPACE TEXTURES
+https://forum.unity.com/threads/mapping-texture-to-world-position-instead-of-object-position.94766/
+
+https://blog.selfshadow.com/sandbox/ltc.html square light
+
+https://gist.github.com/galek/53557375251e1a942dfa  PBR in GLSL
+
+https://threejs.org/examples/#webgl_loader_gltf
+
+
+https://github.com/oframe/ibl-converter/blob/master/src/shaders/PBRShader.js GREAT SIMPLE IMAGE BASED LIGHT PBR SHADER
+
+https://learnopengl.com/PBR/Theory
+https://github.com/KhronosGroup/glTF-Sample-Viewer
+https://github.com/KhronosGroup/glTF-Sample-Viewer/tree/glTF-WebGL-PBR
+https://github.com/KhronosGroup/glTF-Sample-Viewer#physically-based-materials-in-gltf-20
+
+https://oframe.github.io/ogl/examples/?src=load-gltf.html
+https://github.com/oframe/ogl/blob/master/examples/load-gltf.html // Krono PBR Shader in source.
+
+https://github.com/toji/webgpu-test  Multiple Lights and PBR
+
+https://2pha.com/demos/threejs/shaders/single_color_point_lights.html // Simple Multiple Point Lights, good.
+https://2pha.com/demos/threejs/shaders/2_color_fresnel.html
+https://2pha.com/demos/threejs/shaders/fresnel_cube_env.html
+https://2pha.com/demos/threejs/shaders/fresnel_cube_env_2.html
+
+https://2pha.com/demos/threejs/shaders/cel.html Nice Toon Shader
+https://2pha.com/demos/threejs/shaders/cel2.html
+
+
+https://catlikecoding.com/unity/tutorials/custom-srp/point-and-spot-lights/
+
+
+https://blog.demofox.org/2017/01/09/raytracing-reflection-refraction-fresnel-total-internal-reflection-and-beers-law/ Lots of Shader stuff, has GLSL
+
+https://threejs.org/examples/webgl_materials_envmaps_hdr.html IBL, using env map of lighting
+
+//--------------------------------------
+https://www.youtube.com/watch?v=TpWI2rU8iF0  TOON SHADER, multi parts. Even has shiny TOON
+https://www.youtube.com/watch?v=kxWWBmIUxbc  AWESOME TOON HAIR SHADER
+
+https://docs.blender.org/manual/en/latest/render/shader_nodes/shader/diffuse.html
+https://www.learnopengles.com/tag/lambertian-reflectance/
+
+
+https://threejs.org/docs/index.html#api/en/materials/MeshPhysicalMaterial
+https://www.clicktorelease.com/blog/creating-spherical-environment-mapping-shader/
+
+https://www.clicktorelease.com/code/spherical-normal-mapping/#
+
+https://www.clicktorelease.com/code/cross-hatching/
+
+
+// TOON SHADING
+https://alexanderameye.github.io/simple-toon.html
+https://twitter.com/csims314/status/1244391593127424002
+
+https://www.patreon.com/posts/quick-game-art-21398935  toon metal
